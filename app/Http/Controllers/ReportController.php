5d35ee7bee944ac83e5962ad60479d84c1936f2d
Haidar Zamzam <haidar.zamzam99@gmail.com>
2023-08-15 05:46:58 +0700

    move transaction report controller

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Transaction;
 use Illuminate\Http\Request;
 
 class ReportController extends Controller
@@ -9,5 +10,14 @@ class ReportController extends Controller
     public function index()
     {
         return view('report.index');
+    }
+
+    public function getTransactionByType(string $type)
+    {
+        $data = Transaction::select('trans_no', 'trans_date', 'customer')
+            ->where('trans_type', $type)
+            ->get();
+        
+        return ['data' => $data];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ReportController;
 use App\Http\Controllers\SerialNumberController;
 use App\Http\Controllers\TransactionController;
 use Illuminate\Http\Request;
@@ -23,3 +24,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 // add auth check middleware
 Route::get('/serial/{product_id}', [SerialNumberController::class, 'getByProduct'])->name('api.serial');
 Route::get('/transaction/{type}', [TransactionController::class, 'getByType'])->name('api.transaction');
+
+Route::controller(ReportController::class)
+    ->prefix('/report')
+    ->as('api.report.')
+    ->group(function() {
+        Route::get('/transaction/{type}', 'getTransactionByType');
+    });
